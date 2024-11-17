0bf7ee92c599bea318dea4cbaf2189ce3da680ff
Pranav Swaroop M N <115005957+Pranav10ss@users.noreply.github.com>
2024-11-17 21:34:42 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('<h1>Auto Scaling Demo App</h1> <h4>Message: Success</h4> <p>Version: 1.0.0</p>');
 })
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Demo app is up and listening to port ${port}`);
 })
