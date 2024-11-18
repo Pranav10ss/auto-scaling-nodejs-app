23e09d7c82fae710854d9d785399b87db7bfa6c5
Pranav Swaroop M N <115005957+Pranav10ss@users.noreply.github.com>
2024-11-18 21:04:03 +0900

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Auto Scaling Demo App</h1> <h4>Message: Success</h4> <p>Version: 1.0.0</p>');
+  res.send('<h1>EC2 Auto Scaling demo v1.0.0</h1>');
 })
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Demo app is up and listening to port ${port}`);
+app.listen(port, () => {
+  console.log(`Server listening on http://localhost:${port}`);
 })
